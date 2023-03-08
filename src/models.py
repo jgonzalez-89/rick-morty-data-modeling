06f4fe4b77719494f1448b2e769501313dea6e6f
Jose L. Gonzalez Diaz <kbcerverjo@gmail.com>
2023-03-08 16:42:11 +0000

    Create diagram.png

--- src/models.py
+++ src/models.py
@@ -8,26 +8,46 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), unique=True, nullable=False)
+    password = Column(String(8), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class UserFavorites(Base):
+    __tablename__ = 'user_favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    user = relationship(User)
+    character = relationship('Character')
 
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80))
+    status = Column(String(80))
+    species = Column(String(80))
+    character_type = Column(String(80))
+    gender = Column(String(80))
+    image = Column(String(150))
+    
+class Episode(Base):
+    __tablename__='episode'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80))
+    air_date = Column(String(80))
+    episode_number = Column(String(80))
+
+class CharacterEpisode(Base):
+    __tablename__='character_episode'
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer,  ForeignKey('character.id'))
+    episode_id = Column(Integer, ForeignKey('episode.id'))
+    character = relationship(Character)
+    episode = relationship(Episode)
+    
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
